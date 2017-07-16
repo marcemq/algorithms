87d9e1fa1ad4c1884a989d93dc87536e93419c34
Marcela Morales Q <marcela.morales.quispe@gmail.com>
2017-07-15 21:24:44 -0500

    Added Fibonacci iterative function.

--- recursion/fibonacci.py
+++ recursion/fibonacci.py
@@ -8,7 +8,16 @@ def recFibonacci(n):
     else:
         return recFibonacci(n-2) + recFibonacci(n-1)
 
+def itFibonacci(n):
+    i, a, b = 0, 0, 1
+    while i != n:
+        a, b = b, a+b
+        i += 1
+    return a
+
 if __name__ == "__main__":
     n = int(input())
     fn = recFibonacci(n)
+    print(fn)
+    fn = itFibonacci(n)
     print(fn)
