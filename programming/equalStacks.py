827d37426824d2947d4d2b3b57bd24aa1c95c7d2
Marcela Morales Q <marcela.morales.quispe@gmail.com>
2017-08-11 01:10:33 -0500

    Saving only sum of heights on stacks to solution of same maximum heights

--- programming/equalStacks.py
+++ programming/equalStacks.py
@@ -3,45 +3,45 @@
 # https://www.hackerrank.com/challenges/equal-stacks
 
 import sys
-from operator import itemgetter
 
 def checkStackHeight(s1, s2, s3):
     n1, n2, n3 = len(s1), len(s2), len(s3)
     if n1 == 0 or n2 == 0 or n3 == 0:
         return True, 0
-    elif s1[n1-1][1] == s2[n2-1][1] and s2[n2-1][1] == s3[n3-1][1]:
-        return True, s1[n1-1][1]
+    elif s1[n1-1] == s2[n2-1] and s2[n2-1] == s3[n3-1]:
+        return True, s1[n1-1]
     else:
         return False, -1
 
 def removeTopHigherStacks(stackMinTop, s1, s2, s3):
-    if s1[len(s1)-1][1] > stackMinTop[1]:
+    if s1[len(s1)-1] > stackMinTop:
         s1.pop()
-    if s2[len(s2)-1][1] > stackMinTop[1]:
+    if s2[len(s2)-1] > stackMinTop:
         s2.pop()
-    if s3[len(s3)-1][1] > stackMinTop[1]:
+    if s3[len(s3)-1] > stackMinTop:
         s3.pop()
 
-def convertToStack(si, hi, n):
-    height_i = 0
-    hi = list(reversed(hi))
-    for i in range(n):
-        height_i += hi[i]
-        si.append((hi[i], height_i))
+def convertToStack(si, hi):
+    height_sum = 0
+    for height in reversed(hi):
+        height_sum += height
+        si.append(height_sum)
+
+def convertAllToStack(h1, h2, h3, s1, s2, s3):
+    convertToStack(s1, h1)
+    convertToStack(s2, h2)
+    convertToStack(s3, h3)
 
 def getEqualHeight(n, h1, h2, h3):
     s1, s2, s3 = list(), list(), list()
-    convertToStack(s1, h1, n[0])
-    convertToStack(s2, h2, n[1])
-    convertToStack(s3, h3, n[2])
-
+    convertAllToStack(h1, h2, h3, s1, s2, s3)
     while True:
         n1, n2, n3 = len(s1), len(s2), len(s3)
         sameHeightFlag, height = checkStackHeight(s1, s2, s3)
         if sameHeightFlag:
             return height
         else:
-            stackMinTop = min(s1[n1-1],s2[n2-1],s3[n3-1],key=itemgetter(1))
+            stackMinTop = min(s1[n1-1], s2[n2-1], s3[n3-1])
             removeTopHigherStacks(stackMinTop, s1, s2, s3)
 
 if __name__ == "__main__":
